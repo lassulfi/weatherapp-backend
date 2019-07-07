bf77a5475de8fc987030afedfcf2d910227786c5
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-07 14:18:34 -0300

    Liberado CrossOrigin nos endpoints

--- src/main/java/com/weatherapp/resources/CityResource.java
+++ src/main/java/com/weatherapp/resources/CityResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.weatherapp.services.CityService;
 
 @RestController
 @RequestMapping(value="/cidades")
+@CrossOrigin(origins = "*")
 public class CityResource {
 
 	@Autowired
